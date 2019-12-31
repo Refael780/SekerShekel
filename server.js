64c3a20793fab4149b4c79959c212bec9581916e
Refael Huri <refael780@gmail.com>
2019-12-31 12:50:56 +0200

    creates Routes and User Schema

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 const app = express();
+connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
+
+///Define Routes
+app.use('/api/users', require('./DEVCONNECTOR/api/users'));
+app.use('/api/auth', require('./DEVCONNECTOR/api/auth'));
+app.use('/api/profile', require('./DEVCONNECTOR/api/profile'));
+app.use('/api/seker', require('./DEVCONNECTOR/api/seker'));
+app.use('/api/sekers', require('./DEVCONNECTOR/api/sekers'));
 
 const PORT = process.env.PORT || 5000;
 
