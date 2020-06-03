e1487752c0a7820bad5cb3a2b95822295ee60d2d
Refael Huri <refael780@gmail.com>
2020-06-03 21:08:40 +0300

    fade in fet

--- client/src/Components/Pages/Main.js
+++ client/src/Components/Pages/Main.js
@@ -13,6 +13,7 @@ import { loadAllSurvey } from '../../action/survey';
 import Numbers from '../Numbers/Numbers';
 import RegForUpdate from '../RegForUpdate/RegForUpdate';
 import { Animated } from 'react-animated-css';
+import FadeInSection from '../UI/FadeInSection/FadeInSection';
 class Main extends Component {
   componentDidMount = () => {
     this.props.loadAllSurvey();
@@ -32,138 +33,158 @@ class Main extends Component {
     return (
       <Fragment>
         <Container fluid style={{ margin: '0.2rem' }}>
-          <CustomModal />
+          <FadeInSection>
+            <CustomModal />
+          </FadeInSection>
+          <FadeInSection>
+            <Row>
+              <Col className='shd'>
+                <CaruselSeker />
+              </Col>
+            </Row>
+          </FadeInSection>
+          <FadeInSection>
+            <Row className='borderSwitchPicture'>
+              <Col className='info' md='12'>
+                <Container
+                  style={{
+                    fontSize: '1.7rem',
+                    height: '15rem',
+                    padding: ' 1rem',
+                    textAlign: 'center'
+                  }}
+                >
+                  ש המון גרסאות זמינות לפסקאות של Lorem Ipsum. אבל רובם עברו
+                  שינויים בצורה זו או אחרת, על ידי השתלת הומור או מילים אקראיות
+                  שלא נראות אפילו מעט אמינות. אם אתה הולך להשתמש במקטעים של של
+                  ינויים בצורה זו או אחרת, על ידי השתלת הומור או מילים אקראיות
+                  שלא נראות אפילו מעט אמינות. אם אתה הולך להשתמש במקטעים של של
+                  Lorem Ipsum אתה צריך להיות בטוח שאין משהו מביך חבוי בתוך הטקס
+                </Container>
+              </Col>
+              <Col>
+                <FadeInSection>
+                  <RegisterCustom></RegisterCustom>
+                </FadeInSection>
+              </Col>
+            </Row>
+          </FadeInSection>
+          <FadeInSection>
+            <Row>
+              <Col>
+                <div
+                  style={{
+                    fontFamily: 'Amatic SC ,cursive',
+                    textAlign: 'center'
+                  }}
+                >
+                  {' '}
+                  <p style={{ fontSize: '4rem' }}>סקרים שטרם יסתיימו</p>
+                </div>
+              </Col>
+            </Row>
+          </FadeInSection>
 
-          <Row>
-            <Col className='shd'>
-              <CaruselSeker />
-            </Col>
-          </Row>
-          <Row className='borderSwitchPicture'>
-            <Col className='info' md='12'>
-              <Container
-                style={{
-                  fontSize: '1.7rem',
-                  height: '15rem',
-                  padding: ' 1rem',
-                  textAlign: 'center'
-                }}
-              >
-                ש המון גרסאות זמינות לפסקאות של Lorem Ipsum. אבל רובם עברו
-                שינויים בצורה זו או אחרת, על ידי השתלת הומור או מילים אקראיות
-                שלא נראות אפילו מעט אמינות. אם אתה הולך להשתמש במקטעים של של
-                ינויים בצורה זו או אחרת, על ידי השתלת הומור או מילים אקראיות שלא
-                נראות אפילו מעט אמינות. אם אתה הולך להשתמש במקטעים של של Lorem
-                Ipsum אתה צריך להיות בטוח שאין משהו מביך חבוי בתוך הטקס
-              </Container>
-            </Col>
-            <Col>
-              <RegisterCustom></RegisterCustom>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <div
-                style={{
-                  fontFamily: 'Amatic SC ,cursive',
-                  textAlign: 'center'
-                }}
-              >
+          <FadeInSection>
+            <Row>
+              <Col className='bigger' style={{ margin: '0.6rem' }}>
+                <NotFInsh index={0} />
+              </Col>
+              <Col className='bigger' style={{ margin: '0.6rem' }}>
+                <NotFInsh index={1} />
+              </Col>
+              <Col className='bigger' style={{ margin: '0.6rem' }}>
                 {' '}
-                <p style={{ fontSize: '4rem' }}>סקרים שטרם יסתיימו</p>
-              </div>
-            </Col>
-          </Row>
-          <Row>
-            <Col className='bigger' style={{ margin: '0.6rem' }}>
-              <NotFInsh index={0} />
-            </Col>
-            <Col className='bigger' style={{ margin: '0.6rem' }}>
-              <NotFInsh index={1} />
-            </Col>
-            <Col className='bigger' style={{ margin: '0.6rem' }}>
-              <NotFInsh index={2} />
-            </Col>
-          </Row>
+                <NotFInsh index={2} />
+              </Col>
+            </Row>
+          </FadeInSection>
           <br />
-          <hr />
-          <Row>
-            <Col md='8'>
-              <CaruselSeker />
-            </Col>
-            <Col md='4'>
-              <Button
-                style={{ alignItems: 'center' }}
-                color='primary'
-                size='lg'
-                block
-                outline
-              >
-                הזמן סקר
-              </Button>
-              <Button
-                onClick={prestRegForUpdateHandler}
-                style={{ alignItems: 'center' }}
-                color='primary'
-                size='lg'
-                block
-                outline
-              >
-                הרשם לקבלת עדכונים
-              </Button>
-              <br />
 
-              {this.state.RegForUpdateToggel ? <RegForUpdate /> : <div></div>}
-            </Col>
-          </Row>
-          <Row
-            style={{
-              float: 'left',
-              margin: 0,
-              padding: 0
-            }}
-          >
-            <Col
-              xs='12'
-              sm='6'
-              lg='3'
-              className='bigger'
-              style={{ float: 'left', margin: 0, padding: 0 }}
+          <br />
+
+          <hr />
+          <FadeInSection>
+            <Row>
+              <Col md='8'>
+                <CaruselSeker />
+              </Col>
+              <Col md='4'>
+                <Button
+                  style={{ alignItems: 'center' }}
+                  color='primary'
+                  size='lg'
+                  block
+                  outline
+                >
+                  הזמן סקר
+                </Button>
+                <Button
+                  onClick={prestRegForUpdateHandler}
+                  style={{ alignItems: 'center' }}
+                  color='primary'
+                  size='lg'
+                  block
+                  outline
+                >
+                  הרשם לקבלת עדכונים
+                </Button>
+                <br />
+
+                {this.state.RegForUpdateToggel ? <RegForUpdate /> : <div></div>}
+              </Col>
+            </Row>
+          </FadeInSection>
+          <FadeInSection>
+            <Row
+              style={{
+                float: 'left',
+                margin: 0,
+                padding: 0
+              }}
             >
-              <Numbers />
-              <hr />
-            </Col>
-            <Col
-              xs='12'
-              sm='6'
-              lg='3'
-              className='bigger'
-              style={{ float: 'left', margin: '0', padding: 0 }}
-            >
-              <Numbers />
-              <hr />
-            </Col>
-            <Col
-              xs='12'
-              sm='6'
-              lg='3'
-              className='bigger'
-              style={{ float: 'left', margin: '0', padding: 0 }}
-            >
-              <Numbers />
-              <hr />
-            </Col>
-            <Col
-              xs='12'
-              sm='6'
-              lg='3'
-              className='bigger'
-              style={{ float: 'left', margin: '0', padding: 0 }}
-            >
-              <Numbers />
-              <hr />
-            </Col>
-          </Row>
+              <Col
+                xs='12'
+                sm='6'
+                lg='3'
+                className='bigger'
+                style={{ float: 'left', margin: 0, padding: 0 }}
+              >
+                <Numbers />
+                <hr />
+              </Col>
+              <Col
+                xs='12'
+                sm='6'
+                lg='3'
+                className='bigger'
+                style={{ float: 'left', margin: '0', padding: 0 }}
+              >
+                <Numbers />
+                <hr />
+              </Col>
+              <Col
+                xs='12'
+                sm='6'
+                lg='3'
+                className='bigger'
+                style={{ float: 'left', margin: '0', padding: 0 }}
+              >
+                <Numbers />
+                <hr />
+              </Col>
+              <Col
+                xs='12'
+                sm='6'
+                lg='3'
+                className='bigger'
+                style={{ float: 'left', margin: '0', padding: 0 }}
+              >
+                <Numbers />
+                <hr />
+              </Col>
+            </Row>
+          </FadeInSection>
           <hr />
           <br />
         </Container>
